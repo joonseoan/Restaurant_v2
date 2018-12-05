0f0e536921d39c3cf6cd6ccdc61285fac9fe989a
Joon <33235661+joonseoan@users.noreply.github.com>
2018-12-05 00:18:42 -0500

    Fixed key  error

--- sendgrid_webhook.js
+++ sendgrid_webhook.js
@@ -2,5 +2,3 @@ var localtunnel = require("localtunnel");
 localtunnel(9000, { subdomain: "ouafjoonsan" }, function(err, tunnel) {
   console.log("LT running");
 });
-
-// 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
